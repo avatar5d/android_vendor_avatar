38a7419c3a274ae790ab83f905ab28514715f907
avatar <avatar.android@gmail.com>
2010-05-08 00:49:26 -0700

    much improved top-level make script with new options.

--- make.sh
+++ make.sh
@@ -1,23 +1,49 @@
-if [ -z "$1" ]; then
-  echo "usage: make <distilled-src-root> [dst-update.zip-dir]"
+for i in $*
+do
+  case $i in
+    --src=*)
+      SRC=`echo $i | sed 's/.*=//'`
+      ;;
+    --dst=*)
+      DST=`echo $i | sed 's/.*=//'`
+      ;;
+    --no-kernel)
+      NOKERNEL=1
+  esac
+done
+
+if [[ -z "$SRC" ]]; then
+  echo "usage: make --src=/path/to/distilled/ [--dst=/path/to/update.zip/] [--no-kernel]"
   exit 0
 fi
 
-ROOT=$1
-OUT=$2
-SUROOT=$ROOT/system/extras/su
-VENDOR=$ROOT/vendor/avatar
+if [[ ! -d $SRC ]]; then
+  echo "Invalid source directory specified"
+  exit
+fi
+
+if [[ $DST ]]; then
+  if [[ ! -d $DST ]]; then
+    echo "Invalid destination directory specified"
+    exit
+  fi
+fi
+
+SUROOT=$SRC/system/extras/su
+VENDOR=$SRC/vendor/avatar
 
 # prefer the su created by the Superuser app
 if [ -f $SUROOT/Android.mk ]; then
   mv $SUROOT/Android.mk $SUROOT/Android.mk.dead
 fi
 
-# build the bits
-$VENDOR/scripts/makeplatform.sh $ROOT
-$VENDOR/scripts/makekernel.sh $ROOT
+$VENDOR/scripts/makeplatform.sh $SRC
+
+if [[ -z $NOKERNEL ]]; then
+  $VENDOR/scripts/makekernel.sh $SRC
+fi
 
 # create update.zip
-if [ $OUT ]; then
-  $VENDOR/scripts/collectandsign.sh $ROOT $OUT
+if [ $DST ]; then
+  $VENDOR/scripts/collectandsign.sh $SRC $DST
 fi
